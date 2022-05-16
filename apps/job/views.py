4326532e8a0d482636fd6a3f8c15fdbbfbbc6318
castela0119 <castela0119@gmail.com>
2022-05-16 17:45:20 +0900

    temp : #1 json API 작업중

--- apps/job/views.py
+++ apps/job/views.py
@@ -15,3 +15,32 @@ class JsonAPI(APIView):
 
         # print(json.dumps(json_data))
         return Response(json_data)
+
+    def post(self, request):
+
+        # json_data = request.data
+        request = json.loads(request.body)
+
+        # print(json_object)
+        # print(json_object['job_id'])
+        # print(json_object['job_name'])
+        # print(json_object['task_list'])
+
+        with open('job.json', 'r+', encoding='utf-8') as f:
+            json_data = json.load(f)
+            print(f"json_data 입니다. {json_data}")
+            print(json_data['job'])
+            # json_obj['job'].append(json_data)
+            
+            # json_data['job'].append({
+            #     "job_id": json_object['job_id'],
+            #     "job_name": json_object['job_name'],
+            #     "task_list": json_object['task_list'],
+            #     "property": json_object['property']
+            # })
+
+            # json_obj = json.load(f, indent=2)
+            # print(f"json_obj 입니다 {json_obj}")
+            # json_obj['job'].append(json_data)
+        
+        return Response('성공')
