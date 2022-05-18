2eeb81b41c67017f3b72dbc77a5a00dfa01c3494
castela0119 <castela0119@gmail.com>
2022-05-18 13:41:32 +0900

    feat : #1 json data PATCH

--- apps/job/views.py
+++ apps/job/views.py
@@ -21,24 +21,33 @@ class JsonAPI(APIView, JobExecutor):
 
     def post(self, request):
 
-        # print(f"request입니다, :  {request}")
+        input_job = json.loads(request.body)
 
-        request = json.loads(request.body)
+        jobs = je._read_all_job()
+        jobs.append(input_job)
 
-        with open('job.json', 'r', encoding='utf-8') as rf:
-            json_data = json.load(rf)
-            print(f"json_data, :  {json_data}")
-            with open('job.json', 'w', encoding='utf-8') as wf:
-                json_data.append({
-                    "job_id": request['job_id'],
-                    "job_name": request['job_name'],
-                    "task_list": request['task_list'],
-                    "property": request['property']
-                })
+        return Response(jobs)
 
-                json.dump(json_data, wf, indent=4)
+    # def post(self, request):
+
+    #     # print(f"request입니다, :  {request}")
+
+    #     request = json.loads(request.body)
+
+    #     with open('job.json', 'r', encoding='utf-8') as rf:
+    #         json_data = json.load(rf)
+    #         print(f"json_data, :  {json_data}")
+    #         with open('job.json', 'w', encoding='utf-8') as wf:
+    #             json_data.append({
+    #                 "job_id": request['job_id'],
+    #                 "job_name": request['job_name'],
+    #                 "task_list": request['task_list'],
+    #                 "property": request['property']
+    #             })
+
+    #             json.dump(json_data, wf, indent=4)
         
-        return Response('성공')
+    #     return Response('성공')
 
 
 class JsonDetailAPI(APIView, JobExecutor):
@@ -46,5 +55,13 @@ class JsonDetailAPI(APIView, JobExecutor):
     def get(self, request, job_id=None):
 
         job = je._read_job(job_id)
+
+        return Response(job)
+
+    def patch(self, request, job_id=None):
+
+        input_job = json.loads(request.body)
+
+        job = je.edit(job_id, input_job)
 
         return Response(job)
