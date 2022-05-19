9e51453434b48555fa31eeda62fadc172402a84e
do-not-do-that <62286464+do-not-do-that@users.noreply.github.com>
2022-05-19 12:33:38 +0900

    fix : config.urls 수정

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('job.urls')),
+    path('api/v1/jobs', include('job.urls')),
 ]
