7e56563a080d72490a1433c157074676262c766f
do-not-do-that <62286464+do-not-do-that@users.noreply.github.com>
2022-05-19 12:34:20 +0900

    fix : job.urls 수정

--- job/urls.py
+++ job/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import JobDetailRUDViews, JobListView, JobTaskView
 
 urlpatterns = [
-    path('jobs', JobListView.as_view()),
-    path('jobs/<int:id>', JobDetailRUDViews.as_view()),
-    path('jobs/<int:id>/run', JobTaskView.as_view())
+    path('', JobListView.as_view()),
+    path('/<int:id>', JobDetailRUDViews.as_view()),
+    path('/<int:id>/run', JobTaskView.as_view())
 ]
